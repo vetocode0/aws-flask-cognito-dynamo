fe2230f80198a2f79e097205d22b2410aaa02151
Vodrie Cohen <contagious.thoughts@gmail.com>
2023-08-23 09:45:11 -0400

    Setting up infrastructure for app. TODO set up appsync integration with cognito.

---
+++ infra/main.tf
@@ -0,0 +1,27 @@
+terraform {
+    backend "s3" {}
+}
+
+provider "aws" {
+    region = var.aws_region
+}
+
+data "aws_caller_identity" "current" {}
+
+module "api_gateway" {
+    source = "./api_gate"
+    project_name = var.project_name
+    stage_name = var.stage_name
+}
+
+module "cognito" {
+    source = "./cognito"
+    url = module.api_gateway.flask_url
+    project_name = var.project_name
+}
+
+module "s3_buckets" {
+    source = "./s3_buckets"
+    project_name = var.project_name
+    aws_region = var.aws_region
+}
